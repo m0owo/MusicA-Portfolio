7c68d38de6436d3b62ef773a13bca3631ce9eec2
m0owo <8breadsticks8@gmail.com>
2024-01-07 21:42:50 +0700

    with park pic

--- src/components/specific/WindowFrame/index.jsx
+++ src/components/specific/WindowFrame/index.jsx
@@ -3,7 +3,7 @@ import { PiStarFourThin } from "react-icons/pi";
 
 const WindowFrame = () => {
     return (
-        <div className="transition-all ease-linear">
+        <div className="transition-all ease-linear sm:mx-auto sm:mr-10 mt-10">
             <div className="relative w-[201px] h-[300px]
                         rounded-t-[50%] sm:mt-10 m-auto
                         transition-all border-[1px] sm:w-[300px] sm:h-[450px]
@@ -15,12 +15,18 @@ const WindowFrame = () => {
                         transition-all border-[1px]
                         border-black transparent"
                 >
-                    <div className="w-[201px] h-[300px] ml-1 sm:w-[300px] sm:h-[450px]
-                            rounded-t-[50%] bg-[url('/assets/Images/PIC1.jpg')]
+                    <div className="w-[201px] h-[300px] ml-1
+                            rounded-t-[50%] shadow-xl
                             mt-0.5 transition-all border-[1px]
-                            border-gray-700 bg-contain bg-left-bottom z-10"
+                            border-gray-700 bg-contain bg-left-bottom"
                     >
                         <PiStarFourFill className="fill-[#0a063d]" />
+                        <div className="bg-[url('../public/assets/Images/PIC3.png')]
+                            w-[169px] h-[278px]
+                            mb-10 transition-all bg-contain bg-left-bottom"
+                        >
+                            .
+                        </div>
                     </div>       
                     <PiStarFourFill className="w-5 mt-[1px] ml-[220px] sm:ml-[320px] fill-[#0a063d]"/> 
                     <PiStarFourThin className="w-6 ml-[205px] sm:ml-[300px]"/>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -18,14 +18,11 @@ const Home = () => {
                 {/* <SideBar /> */}
                 <NavBar />
                 <ToTopButton />
-                <div className="mt-20 sm:mt-10 flex flex-col md:flex-row justify-center mx-auto" >
-                    <motion.div className="m-auto md:mr-0 
-                                           transition-all">
-                        <WindowFrame />
-                    </motion.div>
-                    <motion.div className="mt-10 md:mt-20 sm:m-auto font-rosie-brown text-5xl 
-                                           sm:text-7xl text-center sm:text-start sm:mx-auto
-                                           transition-all md:text-8xl"
+                <div className="flex flex-col sm:flex-row justify-center m-auto 
+                                sm:mx-14 md:gap-20 transition-all" >
+                    <WindowFrame />
+                    <motion.div className="mt-20 font-rosie-brown text-5xl sm:text-7xl
+                                           sm:mt-40 sm:text-start sm:mx-auto sm:ml-0"
                                 initial={{opacity:0, transition: {duration: 0.1}}}
                                 animate={{opacity:1}}
                                 exit={{opacity:0, transition: {duration: 0.1}}}
